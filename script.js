167724cbd7b56aa56353a6649269753fec8382ba
Fernando Labandeira <nandolabandeira15@gmail.com>
2023-09-07 19:16:47 -0300

    En funcionamiento

---
+++ script.js
@@ -0,0 +1,38 @@
+document.getElementById('registro').addEventListener('submit', function (e) {
+    
+    e.preventDefault()
+
+    
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value
+    const fechaNacimiento = document.getElementById('fechaNacimiento').value
+
+    // Creo un objeto con los datos del formulario
+    const data = {
+        nombre: nombre,
+        apellido: apellido,
+        fechaNacimiento: fechaNacimiento
+    };
+
+    // Realiza la solicitud HTTP POST 
+    fetch('https://jsonplaceholder.typicode.com/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        // Muestra el estado de la respuesta
+        document.getElementById('respuesta').textContent = `Estado del servidor: ${response.status} - ${response.statusText}`;
+        return response.json()
+    })
+    .then(data => {
+        // Muestra la respuesta del servidor
+        console.log(data)
+    })
+    .catch(error => {
+        
+        console.error('Error:', error)
+    })
+});
